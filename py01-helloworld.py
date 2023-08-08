c7428ab0cd16e72949e610dc1b89ce97e7c4d8e2
mattesbszw <107214376+mattesbszw@users.noreply.github.com>
2023-08-08 14:30:29 +0200

    first commit

---
+++ py01-helloworld.py
@@ -0,0 +1,15 @@
+
+# greetings message
+print("*** Hello World Demo Program ***")
+
+# print message 10 times to console
+for i in range(0, 10):
+    
+    # simple possibility to print message and variable
+    print("Hello World " + str(i))
+    
+    # another possibility
+    print("Hello World", str(i))
+
+# end message
+print("*** End World Demo Program ***")
